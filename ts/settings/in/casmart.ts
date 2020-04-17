7458b3ccf576ad0b8b7c7cf4fad694707d797c36
system13-jkchemical <system13@jkchemical.com>
2020-04-17 16:53:15 +0800

    查询方式修改

--- ts/settings/in/casmart.ts
+++ ts/settings/in/casmart.ts
@@ -4,7 +4,7 @@ import { CasmartPullWrite } from '../../first/converter/casmartPullWrite';
 import config from 'config';
 const promiseSize = config.get<number>("promiseSize");
 
-
+/*
 let pullSql = ` SELECT  r.ID, r.PackageId, zcl_mess.dbo.fc_reCAS(p.CAS) AS CasFormat, p.OriginalId, m.name as BrandName, r.CatalogPrice, r.SalePrice, r.Storage, 
                         p.DescriptionC, p.Description, zcl_mess.dbo.fn_mi_pack_toString(j.packnr,j.quantity,j.unit,'abstract') as Package, 
                         zcl_mess.dbo.Fn_get_delivetime(j.JKCat,'CN') AS Delivetime, r.StateName, r.IsDelete, isnull(p.purity,'N/A') AS Purity, 
@@ -28,34 +28,33 @@ let pullSql = ` SELECT  r.ID, r.PackageId, zcl_mess.dbo.fc_reCAS(p.CAS) AS CasFo
                 INNER JOIN zcl_mess.dbo.productschem pc ON pc.JKID = p.JKID 
                 LEFT  JOIN zcl_mess.dbo.sc_restrict sc ON sc.chemid = pc.chemid `;
 
-//-- (SELECT  CASE when EXISTS(SELECT 1 FROM zcl_mess.dbo.sc_restrict WHERE chemid=pc.chemid)  then 'Yes' ELSE 'No' END ) as IsWX,
-/*
-let pullSql = ` DECLARE @id BIGINT;
+*/
 
+
+let pullSql = ` DECLARE @id BIGINT;
                 SELECT  TOP ${promiseSize} @id=ID 
                 FROM    ProdData.dbo.ThirdPartyPlatformEntryResult
                 WHERE   CustomerUnitOnPlatformId = 'e3f8f71734e84d5ba37d37bbd4d7238a' 
                         AND SalesRegionID = 'CN' 
                         AND ID > @iMaxId;
-
                 SELECT  r.ID, r.PackageId, zcl_mess.dbo.fc_reCAS(p.CAS) AS CasFormat, p.OriginalId, m.name as BrandName, r.CatalogPrice, r.SalePrice, r.Storage, 
                         p.DescriptionC, p.Description, zcl_mess.dbo.fn_mi_pack_toString(j.packnr,j.quantity,j.unit,'abstract') as Package, 
                         zcl_mess.dbo.Fn_get_delivetime(j.JKCat,'CN') AS Delivetime, r.StateName, r.IsDelete, 
                         isnull(p.purity,'N/A') AS Purity, r.ThirdPartyPlatformTemplateTypeId AS Templatetypeid, isnull(p.MF,'N/A') AS MF,
-                        (SELECT  CASE when EXISTS(SELECT 1 FROM zcl_mess.dbo.sc_restrict WHERE chemid=pc.chemid)  then 'Yes' ELSE 'No' END ) as IsWX,
+                        (CASE WHEN sc.chemid IS NULL  then 'No' ELSE 'Yes' END ) as IsWX,
                         isnull((SELECT  TOP 1 cc.ClassCode 
                                 FROM	opdata.dbo.SaleProductProductCategory dd
                                         INNER JOIN opdata.dbo.ProductCategoryLanguage ee ON dd.ProductCategoryID=ee.ProductCategoryID AND ee.LanguageID='zh-CN'
                                         inner join ProdData.dbo.PlatformUnitCategoryWithJKMapping pm on pm.JKCategoryId = ee.ProductCategoryID and pm.PlatformUnitCode = 'casmart'
                                         LEFT JOIN  ProdData.dbo.PlatformUnitProductCategory cc ON pm.unitcategoryid = cc.id AND cc.PlatformUnitCode = pm.PlatformUnitCode 
                                         WHERE dd.SaleProductID= r.productid ),'516') AS CategoryId 
-                FROM    ProdData.dbo.ThirdPartyPlatformEntryResult r 
-                        INNER JOIN zcl_mess.dbo.jkcat j ON j.JKCat = r.PackageId 
-                        INNER JOIN zcl_mess.dbo.products p ON j.JKid = p.JKID 
-                        INNER JOIN zcl_mess.dbo.manufactory m ON m.code = r.BrandId
+                FROM    ProdData.dbo.ThirdPartyPlatformEntryResult r
+                        INNER JOIN zcl_mess.dbo.jkcat j ON j.JKCat = r.PackageId
+                        INNER JOIN zcl_mess.dbo.products p ON j.JKid = p.JKID
                         INNER JOIN zcl_mess.dbo.productschem pc ON pc.JKID = p.JKID
+                        INNER JOIN zcl_mess.dbo.manufactory m ON m.code = r.BrandId
+                        LEFT  JOIN zcl_mess.dbo.sc_restrict sc ON sc.chemid = pc.chemid
                 WHERE r.Id = @id; `;
-*/
 
 export const Casmart: UqInTuid = {
         uq: 'platform/Push',
