96f2650700da964cbbd77c11bd5f59ee31c928b3
unknown <W367@wlrc.com>
2020-12-03 17:13:23 +0800

    政采云对接代码

---
+++ ts/tools/HMACSHA256.ts
@@ -0,0 +1,11 @@
+let crypto = require('crypto');
+function HMACSHA256(stringToSign: ArrayBuffer | SharedArrayBuffer, skey: ArrayBuffer | SharedArrayBuffer) {
+    //创建Buffer实例 传进加密
+    let cskey = Buffer.from(skey);
+    let cstringToSign = Buffer.from(stringToSign);
+    let hash = crypto.default.createHmac('sha256', cskey)
+        .update(cstringToSign)
+        .digest('base64');
+    return hash;
+}
+exports.HMACSHA256 = HMACSHA256;
