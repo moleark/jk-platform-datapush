f36da9ad3a61d15dedc71cdceb1d46b2a66dec82
wanggl <120311355@qq.com>
2021-05-27 13:30:52 +0800

    喀斯玛满一赠一活动,

--- ts/settings/in/casmart.ts
+++ ts/settings/in/casmart.ts
@@ -3,8 +3,21 @@ import { UqInTuid } from "uq-joint";
 import { CasmartPullWrite } from '../../first/converter/casmartPullWrite';
 import config from 'config';
 const promiseSize = config.get<number>("promiseSize");
+/*	
+2021.05.27
+喀斯玛活动 为期一个月 6月份 结束后需要改回来.
+活动内容:如下两个产品,用目录价做满一赠一,因为现在是83折,需要恢复为目录价
+修改方案:1.result表更新这四个包装的 lastupdatetime
+		 2.喀斯玛推送数据 加限制 只要是这四个包装不打折
+			
+	J&K 974643  100ML	196		A019746431_100_ML
+				500ML	256		A019746431_500_ML
+				1L		440		A019746431_1_L
+		167915	100ML	338		A01T060020100
+*/
 
-let pullSql = `SELECT  TOP ${promiseSize} r.ID, r.PackageId, zcl_mess.dbo.fc_reCAS(p.CAS) AS CasFormat, p.OriginalId, m.name as BrandName, r.CatalogPrice, r.SalePrice, 
+let pullSql = `SELECT  TOP ${promiseSize} r.ID, r.PackageId, zcl_mess.dbo.fc_reCAS(p.CAS) AS CasFormat, p.OriginalId, m.name as BrandName, r.CatalogPrice,
+                        CASE WHEN r.PackageId IN ('A019746431_100_ML','A019746431_500_ML','A019746431_1_L','A01T060020100') THEN r.CatalogPrice ELSE r.SalePrice END AS SalePrice,
                         r.Storage, p.DescriptionC, p.Description, zcl_mess.dbo.fn_mi_pack_toString(j.packnr,j.quantity,j.unit,'abstract') as Package, r.StateName, r.IsDelete, 
                         isnull(p.purity,'N/A') AS Purity, r.ThirdPartyPlatformTemplateTypeId AS Templatetypeid, REPLACE(REPLACE(isnull(p.MF,'N/A'),'+',''),'?','') AS MF,
                         zcl_mess.dbo.Fn_get_delivetime(j.JKCat,'CN') AS Delivetime, (CASE WHEN sc.chemid IS NULL  then 'No' ELSE 'Yes' END ) as IsWX, j.jkid,
